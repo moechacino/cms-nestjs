31e984c764d316c057b8ad7301012c710dc54c65
mochacino <lanaksa28@gmail.com>
2024-11-11 20:43:53 +0700

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ForbiddenException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+
+  const whitelists = configService.get<string>('WHITELIST_ORIGIN').split(',');
+  app.enableCors({
+    origin: function (origin, cb) {
+      if (whitelists.indexOf(origin) !== -1 || !origin) {
+        cb(null, true);
+      } else {
+        cb(new ForbiddenException('Not Allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
+
+  await app.listen(configService.get('APP_PORT'));
 }
 bootstrap();
