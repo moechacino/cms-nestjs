64a2efa1150510832d5ea1d6d2420f3585b2fd10
mochacino <lanaksa28@gmail.com>
2024-11-12 20:37:54 +0700

    add ValidationPipe to global

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,9 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Module, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { WinstonModule } from 'nest-winston';
 import winston from 'winston';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { ErrorFilter } from './error.filter';
 
 @Global()
@@ -13,6 +13,15 @@ import { ErrorFilter } from './error.filter';
     {
       provide: APP_FILTER,
       useClass: ErrorFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
     },
   ],
   exports: [PrismaService],
